06419a52e6e8f96c44bb75253e4c13b0bb55a74f
okisanjp <okisan.jp@gmail.com>
2014-10-30 13:53:05 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: chef-recipes-lamp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{
+	httpd php php-mbstring php-gd php-mysql php-pear
+}.each do |package|
+  action :install
+}
+end
+
+service "httpd" do
+  action [ :enable, :start ]
+end
+
+package "mysql-server" do
+  action :install
+end
+
+service "mysqld" do
+  action [ :enable, :start ]
+end
